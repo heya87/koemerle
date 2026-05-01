2c692ffd98ca9152fd9b8bd3598aaeacf33d04a0
heya87 <joel.rolli@ik.me>
2026-05-02 01:28:09 +0200

    more migration fixing

--- check-networks.sh
+++ check-networks.sh
@@ -1,1 +1,2 @@
-sudo docker run --rm --network coolify postgres:18-alpine psql "postgres://postgres:CPKliE0yxmqWk9dmneWeA2ScyZnu340bu0Fx77krEM4gb27wqxtpoklz05GNxUSP@h9oylqsv4zos82vg084zbcux:5432/postgres" -c '\l'
+sudo docker exec h9oylqsv4zos82vg084zbcux psql -U postgres -d postgres -c '\dt public.*'
+sudo docker exec h9oylqsv4zos82vg084zbcux psql -U postgres -d postgres -c '\dn'
