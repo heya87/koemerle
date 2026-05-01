eba56792a4d7d7ba829798b435fc8fb2935ec18a
Joel Rolli <joel.rolli@ergon.ch>
2026-05-02 01:10:46 +0200

    fix migration

---
+++ check-networks.sh
@@ -0,0 +1,1 @@
+sudo docker run --rm --network coolify postgres:18-alpine psql "postgres://postgres:CPKliE0yxmqWk9dmneWeA2ScyZnu340bu0Fx77krEM4gb27wqxtpoklz05GNxUSP@h9oylqsv4zos82vg084zbcux:5432/postgres" -c '\l'
